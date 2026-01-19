17db273041269e400b2a8681b1a0ec563319414d
Kutty <godwinraj845@gmail.com>
2026-01-20 00:48:06 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
 
+const HOST = process.env.HOST || '0.0.0.0'
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT,()=>{
